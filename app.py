854a934b746875080439970a3183a84fe91edeac
jwong <jwong@student.42.fr>
2016-10-19 12:13:11 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+import flask from Flask
+import apiai
+import json
+
 app = Flask(__name__)
 
-@app.route ('/')
-def index():
-    return render_template("index.html")
+@app.route('/'):
+def hello_world():
+    return (render_template('index.html'))
 
+def main():
+    ai = apiai.ApiAI('68a530a5164d4f4fab7b044398f99ebd')
 
-if __name__ == "__main__":
+    while True:
+        user_input = raw_input("me: ")
+        if user_input == 'exit':
+            print "bot: Thank you and come again"
+            break
+
+        request = ai.text_request()
+        request.query = user_input
+        response = json.loads(request.getresponse().read())
+        result = response['result']
+        fulfillment = result.get("fulfillment")
+        print "bot: " + fulfillment['speech']
+
+if __name__ == '__main__':
     app.run(debug=True)
