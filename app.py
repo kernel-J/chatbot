543656eadd1cebf840fac4251784a8672b69fe1f
jwong <jwong@student.42.fr>
2016-10-19 14:36:53 +0200

    testing deployment

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-import flask from Flask, render_template
+from flask import Flask, render_template, request
 import apiai
 import json
 
 app = Flask(__name__)
+ai = apiai.ApiAI('68a530a5164d4f4fab7b044398f99ebd')
 
-@app.route('/'):
-def hello_world():
-    return (render_template('index.html'))
+@app.route('/', methods=['GET', 'POST'])
+def input():
+    if request.method == 'POST':
+        ret = main(request.form['user_input'])
+        return (echo(ret))
+    else:
+        return (render_template('index.html'))
 
-def main():
+def echo(resp):
+    if resp != '':
+        return (render_template('index.html', ret=resp))
+
+def main(user_input):
+    if user_input == 'exit':
+        return ("Thank you and come again")
+
+    request = ai.text_request()
+    request.query = user_input
+    response = json.loads(request.getresponse().read())
+    result = response['result']
+    fulfillment = result.get("fulfillment")
+    return (fulfillment['speech'])
+
+'''def main():
     ai = apiai.ApiAI('68a530a5164d4f4fab7b044398f99ebd')
 
     while True:
@@ -23,6 +43,7 @@ def main():
         result = response['result']
         fulfillment = result.get("fulfillment")
         print "bot: " + fulfillment['speech']
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
+    #main()
